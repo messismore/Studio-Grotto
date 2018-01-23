b32085d8173bc97971387d5b71dae1a19447d4a8
Felix Deiters <username@users.noreply.github.com>
2018-01-24 00:20:23 +0100

    Add button to toggle layers.

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 
 mapboxgl.accessToken = 'pk.eyJ1IjoibWVzc2lzbW9yZSIsImEiOiJjamF6aDJiNHEwbXBvMzJvNjUwdDdrbzRsIn0.w0i5lPoQtsBo5yeue9lYeQ';
-var map = new mapboxgl.Map({
+const map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/mapbox/light-v9',
     center: [13.41028,  52.52077],
@@ -9,35 +9,55 @@ var map = new mapboxgl.Map({
 
 
 map.on('load', function () {
-
+  map.addSource(
+    'test',
+    {
+      "type": "geojson",
+      "data": "https://messismore.github.io/Studio-Grotto/samples/Test.geojson"
+    },
+  );
   map.addLayer({
-      "id": "route",
+      "id": "test",
       "type": "line",
-      "source": {
-          "type": "geojson",
-          "data": "https://messismore.github.io/Studio-Grotto/samples/Test.geojson"
-          // "data": {
-          //     "type": "Feature",
-          //     "properties": {},
-          //     "geometry": {
-          //         "type": "LineString",
-          //         "coordinates": [
-          //           [13.409510850906372, 52.52224008961698],
-          //           [13.40506911277771, 52.52002051452143],
-          //           [13.407279253005981, 52.518251302292406],
-          //           [13.412128686904907, 52.5205950035378],
-          //           [13.409510850906372, 52.52224008961698]
-          //         ]
-          //       }
-          //     }
-            },
+      "source": "test",
       "layout": {
+        "visibility": "visible",
         "line-join": "round",
-        "line-cap": "round"
+        "line-cap": "round",
       },
       "paint": {
         "line-color": "#888",
-        "line-width": 8
+        "line-width": 8,
       }
   });
 });
+
+var toggleableLayerIds = [ 'test', ];
+
+for (var i = 0; i < toggleableLayerIds.length; i++) {
+    var id = toggleableLayerIds[i];
+
+    var link = document.createElement('a');
+    link.href = '#';
+    link.className = 'active';
+    link.textContent = id;
+
+    link.onclick = function (e) {
+        var clickedLayer = this.textContent;
+        e.preventDefault();
+        e.stopPropagation();
+
+        var visibility = map.getLayoutProperty(clickedLayer, 'visibility');
+
+        if (visibility === 'visible') {
+            map.setLayoutProperty(clickedLayer, 'visibility', 'none');
+            this.className = '';
+        } else {
+            this.className = 'active';
+            map.setLayoutProperty(clickedLayer, 'visibility', 'visible');
+        }
+    };
+
+    var layers = document.getElementById('menu');
+    layers.appendChild(link);
+}
