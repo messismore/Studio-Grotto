7304150f28ae9c9d5486e8861f9144f08a6147ce
Felix Deiters <username@users.noreply.github.com>
2018-03-07 21:04:33 +0100

    Add loading spinner to sidebar

--- main.js
+++ main.js
@@ -26,7 +26,33 @@ const map = new mapboxgl.Map({
  });
 
 
+ function addSpinner(el, static_pos)
+ {
+   console.log('Adding Spinner…');
+   var spinner = el.children('.spinner');
+   if (spinner.length && !spinner.hasClass('spinner-remove')) return null;
+   !spinner.length && (spinner = $('<div class="spinner' + (static_pos ? '' : ' spinner-absolute') + '"/>').appendTo(el));
+   animateSpinner(spinner, 'add');
+ }
 
+ function removeSpinner(el, complete)
+ {
+   console.log("Removing Spinner…");
+   var spinner = el.children('.spinner');
+   spinner.length && animateSpinner(spinner, 'remove', complete);
+ }
+
+ function animateSpinner(el, animation, complete)
+ {
+   if (el.data('animating')) {
+     el.removeClass(el.data('animating')).data('animating', null);
+     el.data('animationTimeout') && clearTimeout(el.data('animationTimeout'));
+   }
+   el.addClass('spinner-' + animation).data('animating', 'spinner-' + animation);
+   el.data('animationTimeout', setTimeout(function() { animation == 'remove' && el.remove(); complete && complete(); }, parseFloat(el.css('animation-duration')) * 1000));
+ }
+
+ addSpinner($('#sidebarleft'))
 
 map.on('load', async function () {
 
@@ -42,6 +68,7 @@ map.on('load', async function () {
     notesLayerObject,
   ];
 
+  removeSpinner($('#sidebarleft'))
   layerObjects.forEach(layerObject => {
 
     // call add() to add them to the map
